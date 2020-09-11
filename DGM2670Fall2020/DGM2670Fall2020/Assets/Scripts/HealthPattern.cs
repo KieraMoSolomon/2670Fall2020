d19cb088a490235d2afa14314e218761bd108f88
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-09-10 23:46:30 -0600

    Working on health and death

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/HealthPattern.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class HealthPattern : MonoBehaviour
+{
+    public FloatData myHeath, startHealth;
+
+    public MoveData deathMovement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myHeath.value = startHealth.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myHeath.value == 0)
+        {
+            Debug.Log("You died");
+            GetComponent<CharacterMovement>().movePattern = deathMovement;
+            //die
+        }
+    }
+}
