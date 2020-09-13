570cef7872bf06f090be083979bb108f502482b3
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-09-12 23:51:21 -0600

    trying to put a spawn on death

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/DeathBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DeathBehavior : MonoBehaviour
+{
+    public SpawnData startPoint;
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = startPoint.valueX;
+    }
+}
