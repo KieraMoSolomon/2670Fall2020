34f053c4be6fb981f86adbab7ea5e173a2743e69
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-09-18 00:18:11 -0600

    Working on adding force in correct rotation

--- DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/ApplyForce.cs
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/ApplyForce.cs
@@ -7,12 +7,15 @@ public class ApplyForce : MonoBehaviour
     private Rigidbody rBody;
 
     public FloatData force;
-    // Start is called before the first frame update
+    public GameObject obj;
+    
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
         var forceDirection = new  Vector3(force.value, 0, 0);
+        var forceRotaion = obj.transform.rotation.eulerAngles;
         //force direction needs to be based on player rotation (hint Scriptable Object)
         rBody.AddRelativeForce(forceDirection);
+        rBody.AddRelativeForce(forceRotaion);
     }
 }
