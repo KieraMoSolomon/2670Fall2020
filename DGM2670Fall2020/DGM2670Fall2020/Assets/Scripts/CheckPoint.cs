e9787a1e6e4bde655ef1c4be1c8315629194b0f7
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-09-21 12:23:13 -0600

    Working on spawn still

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public SpawnData checkPoint;
+    private void OnTriggerEnter(Collider other)
+    {
+        checkPoint.SetValueFromTransform(transform.position);
+    }
+}
--- DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/DeathBehavior.cs
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/DeathBehavior.cs
@@ -3,12 +3,12 @@
 public class DeathBehavior : MonoBehaviour
 {
     public SpawnData startPoint;
-    public Transform obj;
+    public GameObject obj;
     public void OnTriggerEnter(Collider other)
     {  
         Debug.Log("Trigger!!!!");
         Debug.Log(obj.transform.position);
-        obj.transform.position = startPoint.value;
+        obj.transform.position = new Vector3(1f, 0f, 0f);
         Debug.Log(obj.transform.position);
 
         //other.transform.position = startPoint.value;
