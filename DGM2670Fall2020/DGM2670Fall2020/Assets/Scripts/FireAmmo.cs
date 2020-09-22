e9973304859c775b94d9f09f51f9b7dedb80952c
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-09-21 23:58:40 -0600

    class work

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/FireAmmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class FireAmmo : MonoBehaviour
+{
+    public UnityEvent mouseDownEvent;
+    public void OnMouseDown()
+    {
+        mouseDownEvent.invoke();
+    }
+    
+}
