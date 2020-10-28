62b43a9c966bffba4f3e3930d24b9614ad46c780
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-10-27 23:39:24 -0600

    working through 10/7 (AI)

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/Behaviours/AIBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class AIBehaviour : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    public bool canNav = true;
+    private WaitForFixedUpdate wffu;
+    public float holdTime = 1f;
+    private WaitForSeconds wfs;
+    private void Start()
+    {
+        wfs = new WaitForSeconds(holdTime);
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private IEnumerator Navigate()
+    {
+        yield return wfs;
+        canNav = true;
+        while (canNav)
+        {
+            yield return wffu;
+            agent.destination = player.position;
+            print("walking");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canNav = false;
+        StartCoroutine(Navigate());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canNav = false;
+    }
+}
