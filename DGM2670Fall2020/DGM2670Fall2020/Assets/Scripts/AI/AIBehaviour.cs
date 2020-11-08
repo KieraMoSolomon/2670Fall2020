89b13619e46b06c4fb25ca3335529d129a9c5eac
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-08 10:08:01 -0700

    Done with class 10/12

--- DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/AI/AIBehaviour.cs
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/AI/AIBehaviour.cs
@@ -1,27 +1,34 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
+
 [RequireComponent(typeof(NavMeshAgent))]
 public class AIBehaviour : MonoBehaviour
 {
-    private bool canNav;
+    private bool canHunt, canPatrol;
     private NavMeshAgent agent;
     private WaitForFixedUpdate wffu;
     private WaitForSeconds wfs;
-    
+
     public float holdTime = 2f;
     public Transform player;
+    
+    public List<Transform> patrolPoints;
+    private int i = 0;
     private void Start()
     {
         wfs = new WaitForSeconds(holdTime);
         wffu = new WaitForFixedUpdate();
         agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(Patrol());
     }
 
     private IEnumerator OnTriggerEnter(Collider other)
     {
-        canNav = true;
+        canHunt = true;
         agent.destination = player.position;
         var distance = agent.remainingDistance;
         while (distance <= 0.25f)
@@ -30,14 +37,27 @@ public class AIBehaviour : MonoBehaviour
             yield return wffu;
         }
         yield return wfs;
-        if (canNav)
+        if (canHunt)
         {
             StartCoroutine(OnTriggerEnter(other));
         }
+        else
+        {
+            StartCoroutine(Patrol());
+        }
     }
 
+    private IEnumerator Patrol()
+    {
+        canPatrol = true;
+        if (agent.pathPending || !(agent.remainingDistance < 0.5f)) return;
+        agent.destination = patrolPoints[i].position;
+        i = (i + 1) % patrolPoints.Count;
+        yield return wfs;
+    }
+    
     private void OnTriggerExit(Collider other)
     {
-        canNav = false;
+        canHunt = false;
     }
 }
