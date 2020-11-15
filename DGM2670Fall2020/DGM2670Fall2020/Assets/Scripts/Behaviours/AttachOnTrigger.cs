7a4fa835b67882c36b0c8d7cb3b3ce361cb7cfb3
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-15 13:46:54 -0700

    moving platform

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/Behaviours/AttachOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachOnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = transform;
+        //transform.parent = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/Behaviours/InvokeBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InvokeBehaviour : MonoBehaviour
+{
+    public UnityEvent onInvokeEvent;
+    public float holdTime = 3f;
+
+    public void OnInvoke()
+    {
+        Invoke(nameof(InvokeCall), holdTime);
+    }
+
+    private void InvokeCall()
+    {
+        onInvokeEvent.Invoke();
+    }
+}
