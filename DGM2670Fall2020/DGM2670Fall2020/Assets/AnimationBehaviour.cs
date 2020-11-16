80be2e8830cb5278276da24f53de2970f4c09c31
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-16 11:35:30 -0700

    understanding unity actions

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/AnimationBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+public class AnimationBehaviour : MonoBehaviour
+{
+    private Animator anims;
+
+    private void Start()
+    {
+        anims = GetComponent<Animator>();
+        anims.SetTrigger("Left");
+    }
+}
--- DGM2670Fall2020/DGM2670Fall2020/Assets/animsStates.cs
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/animsStates.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 
 public class animsStates : StateMachineBehaviour
 {
-    public UnityEvent stateEnterEvent;
+    public UnityEvent stateEnterEvent, stateExitEvent;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
@@ -19,10 +19,10 @@ public class animsStates : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateExitEvent.Invoke();
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
