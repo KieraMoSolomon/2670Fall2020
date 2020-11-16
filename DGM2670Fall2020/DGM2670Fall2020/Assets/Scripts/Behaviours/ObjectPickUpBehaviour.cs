b3ecda9da9901338c3c3d450fdd7c30b3fb29002
Kiera Solomon <kieramorgensolomon@gmail.com>
2020-11-16 13:32:01 -0700

    push/pull object

---
+++ DGM2670Fall2020/DGM2670Fall2020/Assets/Scripts/Behaviours/ObjectPickUpBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class ObjectPickUpBehaviour : MonoBehaviour
+{
+    private Rigidbody rBody;
+    private bool canPickUp;
+    private void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        canPickUp = Input.GetKeyDown(KeyCode.P);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (canPickUp)
+        {
+            transform.parent = other.transform;
+            rBody.isKinematic = true;
+            rBody.useGravity = false;
+        }
+        else
+            transform.parent = null;
+            rBody.isKinematic = false;
+            rBody.useGravity = true;
+    }
+}
